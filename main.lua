6ebd6dfdc8e56f200504d7e0ef2fccd27ee6afed
Lucien Greathouse <me@lpghatguy.com>
2017-05-07 20:32:53 -0700

    Color constraint example

--- main.lua
+++ main.lua
@@ -1,24 +1,18 @@
 local Rukt = require("rukt")
 local Box = Rukt.Box
 
-local function set(source, key, value)
-	local new = {}
-
-	for key, value in pairs(source) do
-		new[key] = value
-	end
-
-	new[key] = value
-
-	return new
-end
-
 local function Children(...)
 	local children = {...}
 	return function(abstract, concrete)
 		for _, child in ipairs(children) do
 			table.insert(abstract.children, child)
 		end
+	end
+end
+
+local function Color(r, g, b)
+	return function(abstract, concrete)
+		concrete.color = {r, g, b}
 	end
 end
 
@@ -70,7 +64,7 @@ local layout = sizedBox(300, 300,
 			sizedBox(200, 200,
 				Children(
 					sizedBox(100, 50),
-					sizedBox(50, 50),
+					sizedBox(50, 50, Color(255, 0, 0)),
 					sizedBox(100, 50)
 				)
 			)
@@ -83,6 +77,11 @@ local layout = sizedBox(300, 300,
 local concrete = layout:congeal()
 
 local function render(concrete)
+	if concrete.color then
+		love.graphics.setColor(concrete.color)
+	else
+		love.graphics.setColor(255, 255, 255)
+	end
 	love.graphics.rectangle("line", concrete.x, concrete.y, concrete.width, concrete.height)
 
 	for _, child in ipairs(concrete.children) do
